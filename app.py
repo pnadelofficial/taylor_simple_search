a79732e3ba0c36a6e3fcbf3ef5185600a6dec1dc
pnadelofficial <75741790+pnadelofficial@users.noreply.github.com>
2023-08-18 19:55:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -81,6 +81,6 @@ if query_str != '':
             else:
                 st.write(f"No results for this query in the {cat_choice} documents.")  
         else:
-            for i, r in enumerate(results):
+            for i, r in enumerate(results[st.session_state.start:st.session_state.start+st.session_state.to_see]):
                 utils.display_results(i, r, data, searches)
             st.write(f'Page: {st.session_state.page_count} of {len(results)//to_see}')
