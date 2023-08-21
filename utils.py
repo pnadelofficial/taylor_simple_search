4a19dfc543551c43285e1ed3c7e7e36ae9595a2e
peter nadel <peter.m.nadel@gmail.com>
2023-08-21 10:36:14 -0400

    add breaks to transcript data

--- utils.py
+++ utils.py
@@ -72,5 +72,9 @@ def display_results(i, r, data, searches, display_date=True):
     amount = st.number_input('Choose context length', key=f'num_{i}', value=1, step=1, help='This number represents the amount of sentences to be added before and after the result.')
     if st.button('Add context', key=f'con_{i}'):
         full = add_context(data, r, amount)
+    if ('QUESTION:' in full) and ('ANSWER:' in full):
+        print('met')
+        print(re.sub('(?=ANSWER:)', '<br>', full, flags=re.DOTALL))
+        full = re.sub('(?=ANSWER:)', '<br>', full, flags=re.DOTALL)
     st.markdown(inject_highlights(escape_markdown(full.replace('\n --', ' --')), searches), unsafe_allow_html=True) 
     st.markdown("<hr style='width: 75%;margin: auto;'>", unsafe_allow_html=True)
