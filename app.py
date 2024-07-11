8e09fb69c403107c7da71e810f70742e6d8dfe38
peter nadel <peter.m.nadel@gmail.com>
2024-07-11 14:43:23 -0400

    remove fpdf (kept fpdf2)

--- app.py
+++ app.py
@@ -20,6 +20,19 @@ DIRS = [d for d in os.listdir('./indices') if (d != 'transcript_answers_index') 
 DIRS = [d for d in DIRS if ('bydoc' not in d)]
 print(DIRS)
 
+with st.expander('Click for further information on how to construct a query.'):
+    st.markdown("""
+    * If you'd like to search for just a single term, you can enter it in the box above. 
+    * If you'd like to search for a phrase, you can enclose it in quotations, such as "Macfarlane Trust".
+    * A query like "Macfarlane Trust"~5 would return results where "Marcfarlane" and "Trust" are at most 5 words away from each other.
+    * AND can be used as a boolean operator and will return results where two terms are both in a passage. AND is automatically placed in a query of two words, so Macfarlane Trust is internally represented as Macfarlane AND Trust.
+    * OR can be used as a boolean operator and will return results where either one of two terms are in a passage.
+    * NOT can be used as a boolean operator and will return results which do not include the term following the NOT.
+    * From these boolean operators, one can construct complex queries like: HIV AND Haemophilia NOT "Hepatitis C". This query would return results that have both HIV and Haemophilia in them, but do not have Hepatitis C.
+    * Parentheses can be used to group boolean statements. For example, the query Haemophilia AND ("Hepatitis C" OR  HIV) would return results that have Haemophilia and either Hepatitis C or HIV in them. 
+    * If you'd like to search in a specific date range, you can specify it with the date: field. For example, date:[20210101 TO 20220101] HIV would return results between January 1st, 2021 and January 1st, 2022 that have HIV in them.
+    """)
+
 query_str = st.text_input('Search for a word or phrase', on_change=utils.reset_pages)
 choice = st.selectbox('What documents would you like to search in?', DIRS, format_func=lambda x: x.replace('_index', '').replace('_', ' ').replace('docs', '').title()+' documents')
 to_see = st.number_input('How many results would you like to see per page?', value=10)
